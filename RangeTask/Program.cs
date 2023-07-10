c7b3843907701eefb0b2f30b89c9b7587ddc0bc9
sharman83 <autoshildik@yandex.ru>
2023-07-10 23:56:46 +0700

    Add Range.cs

---
+++ RangeTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RangeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 2");
+        }
+    }
+}
