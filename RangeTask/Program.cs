58bfc3168ab46ce80acff27703a147318c4c80bd
sharman83 <autoshildik@yandex.ru>
2023-07-11 20:16:34 +0700

    Добавил класс Range. Написал программу с использованием класса Range.

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -6,7 +6,19 @@ namespace RangeTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! 4");
+            Console.WriteLine("Введите начало числового диапазона:");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конец числового диапазона:");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(from, to);
+
+            Console.WriteLine($"Длина числового диапазона: {range.GetLength()}");
+            Console.WriteLine($"Число принадлежит данному диапазону (true/false): {range.IsInside(number)}");
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -6,5 +6,24 @@ namespace RangeTask
 {
     class Range
     {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
     }
 }
