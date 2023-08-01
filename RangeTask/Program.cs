1d89ccec01445be46be524cdfd65ffab81d6c839
sharman83 <autoshildik@yandex.ru>
2023-08-01 18:32:10 +0700

    Испрален класс Range согласно второй части задачи

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -6,19 +6,55 @@ namespace RangeTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите начало числового диапазона:");
-            double from = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите начало первого числового диапазона:");
+            double from1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите конец числового диапазона:");
-            double to = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите конец первого числового диапазона:");
+            double to1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите число:");
-            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите начало первого числового диапазона:");
+            double from2 = Convert.ToDouble(Console.ReadLine());
 
-            Range range = new Range(from, to);
+            Console.WriteLine("Введите конец первого числового диапазона:");
+            double to2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"Длина числового диапазона: {range.GetLength()}");
-            Console.WriteLine($"Число принадлежит данному диапазону (true/false): {range.IsInside(number)}");
+            Range range1 = new Range(from1, to1);
+            Range range2 = new Range(from2, to2);
+
+            // ищем интервал-пересечения
+            Range intersectionRange = range1.GetIntersection(range2);
+
+            if (intersectionRange == null)
+            {
+                Console.WriteLine("Пересечения нет.");
+            }
+            else
+            {
+                Console.WriteLine($"Интервал-пересечения: {intersectionRange.From} - {intersectionRange.To}");
+            }
+
+            // ищем интервал(ы)-объединения
+            Range[] unionRange = range1.GetUnion(range2);
+
+            foreach (Range range in unionRange)
+            {
+                Console.WriteLine($"Интервал-объединения: {range.From} - {range.To}");
+            }
+
+            // ищем разность интервалов
+            Range[] differenceRange = range1.GetDifference(range2);
+
+            if (differenceRange.Length == 0)
+            {
+                Console.WriteLine("Разности нет.");
+            }
+            else
+            {
+                foreach (Range range in differenceRange)
+                {
+                    Console.WriteLine($"Интервал-разность: {range.From} - {range.To}");
+                }
+            }
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RangeTask
 {
@@ -12,8 +10,8 @@ namespace RangeTask
 
         public Range(double from, double to)
         {
-            From = from;
-            To = to;
+            this.From = from;
+            this.To = to;
         }
 
         public double GetLength()
@@ -21,9 +19,55 @@ namespace RangeTask
             return To - From;
         }
 
-        public bool IsInside(double number)
+        public Range GetIntersection(Range anotherRange)
         {
-            return number >= From && number <= To;
+            double intersectionFrom = Math.Max(From, anotherRange.From);
+            double intersectionTo = Math.Min(To, anotherRange.To);
+
+            if (intersectionFrom >= intersectionTo)
+            {
+                return null;
+            }
+
+            return new Range(intersectionFrom, intersectionTo);
+        }
+
+        public Range[] GetUnion(Range anotherRange)
+        {
+            double unionFrom = Math.Min(From, anotherRange.From);
+            double unionTo = Math.Max(To, anotherRange.To);
+
+            if (anotherRange.From > To)
+            {
+                return new Range[] { new Range(From, To), new Range(anotherRange.From, anotherRange.To) };
+            }
+
+            if (anotherRange.To < From)
+            {
+                return new Range[] { new Range(anotherRange.From, anotherRange.To), new Range(From, To) };
+            }
+
+            return new Range[] { new Range(unionFrom, unionTo) };
+        }
+
+        public Range[] GetDifference(Range anotherRange)
+        {
+            if (From >= anotherRange.From && To <= anotherRange.To)
+            {
+                return new Range[0];
+            }
+
+            if (From >= anotherRange.From && From <= anotherRange.To && To > anotherRange.To)
+            {
+                return new Range[] { new Range(anotherRange.To, To) };
+            }
+
+            if (To >= anotherRange.From && To <= anotherRange.To && From < anotherRange.From)
+            {
+                return new Range[] { new Range(From, anotherRange.From) };
+            }
+
+            return new Range[] { new Range(From, anotherRange.From), new Range(anotherRange.To, To) };
         }
     }
 }
